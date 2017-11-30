1f0b1ea1d0d97c7707edae569d7d20c50071fe7b
answlgn <32009007+answlgn@users.noreply.github.com>
2017-12-01 03:28:28 +0900

    Delete slider.js

--- Final/slider.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
